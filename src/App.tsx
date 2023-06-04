3135b9e5f39b8e7f630dfe6e81a5c3ea9c691504
Lyso95 <amos@langensiepen.net>
2023-06-04 02:00:27 +0200

    Finished Nav -Added @headlessui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import Navigation from "./sections/navbar/navigation.tsx"
+import Mainhero from "./sections/hero/mainhero.tsx"
+import StackedList from "./components/stackedList.tsx"
+
 function App() {
+
 
   return (
     <>
-      <h1>Hello World</h1>
+      <header className="sticky top-0 z-50">
+        <Navigation />
+      </header>
+      <main>
+        <Mainhero />
+        <StackedList/>
+          <div id="about"></div>
+      </main>
     </>
   )
 }
